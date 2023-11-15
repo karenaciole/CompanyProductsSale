1d6d584be78252f79929755040e171f7b63e9429
karen <96853561+karenaciole@users.noreply.github.com>
2023-11-15 13:38:23 -0300

    q1 e q2 done

--- queries.sql
+++ queries.sql
@@ -1,6 +1,31 @@
 -- Valor total das vendas e dos fretes por produto e ordem de venda
+SELECT
+  produtoid,
+  C.data AS data_vendas,
+  clienteid,
+  SUM(valor) AS valor_vendas,
+  SUM(valorfrete) AS valor_frete
+FROM
+  FatoDetalhes_DadosModelagem D
+INNER JOIN FatoCabecalho_DadosModelagem C
+  ON D.cupomid = C.cupomid
+GROUP BY
+  produtoid
+ORDER BY
+	SUBSTR(data_vendas, 7, 4) || '/' || SUBSTR(data_vendas, 4, 2) || '/' || SUBSTR(data_vendas, 1, 2) 
+
 -- Valor de venda por tipo de produto
+-- (não há campo de tipo de produto ou semelhante, entao usei o campo 'produtoid')
+SELECT 
+  DISTINCT(produtoid),
+  SUM(valor) AS venda_valor
+FROM 
+  FatoDetalhes_DadosModelagem 
+GROUP BY 
+  produtoid
+
 -- Quantidade e valor das vendas por dia, mês, ano
+
 -- Lucro dos meses
 -- Venda por produto
 -- Venda por cliente, cidade do cliente e estado
