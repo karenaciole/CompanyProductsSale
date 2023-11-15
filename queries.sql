e1aad9a44a0e2c356d834f5e4db07d9102207966
karen <96853561+karenaciole@users.noreply.github.com>
2023-11-15 10:49:35 -0300

    add questions

---
+++ queries.sql
@@ -0,0 +1,8 @@
+-- Valor total das vendas e dos fretes por produto e ordem de venda
+-- Valor de venda por tipo de produto
+-- Quantidade e valor das vendas por dia, mês, ano
+-- Lucro dos meses
+-- Venda por produto
+-- Venda por cliente, cidade do cliente e estado
+-- Média de produtos vendidos
+-- Média de compras que um cliente faz
